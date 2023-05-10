83a51fcdf7235ca4b2edeb94c8975b5e06e2b782
JoeGuidoboni <joe.guidoboni@gmail.com>
2023-05-09 22:29:03 -0400

    added keyboard card to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import { KeyboardCard } from './components/keyboard-card/keyboard-card';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         hey there
+        <KeyboardCard></KeyboardCard>
       </header>
+      
     </div>
   );
 }
