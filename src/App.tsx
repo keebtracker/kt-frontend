fe37e54e63b440dc9fd6825b4c1ca4c4bc28e8ea
jakebalsamo <balsamicj@yahoo.com>
2023-05-09 23:23:23 -0400

    created initial user auth, added login/logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import LoginButton from './components/login/login'
+import LogoutButton from './components/logout/logout';
 
 function App() {
   return (
     <div className="App">
+      <LoginButton />
+      <LogoutButton />
       <header className="App-header">
         hey there
       </header>
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-swuyfyhogcq3v77v.us.auth0.com"
+    clientId="3mKA4V2VU648q3e6PYsFLbKmNRi099LX"
+    authorizationParams={{
+      redirect_uri: "http://localhost:3000/callback"
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
