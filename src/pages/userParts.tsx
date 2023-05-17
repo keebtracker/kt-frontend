60d4bd2c8b14e6a81eeb070b54c61f9724a50b9e
Ian Rae <ian.rae@snhu.edu>
2023-05-16 23:12:20 -0400

    added tailwind and userparts page

--- src/pages/userParts.tsx
+++ src/pages/userParts.tsx
@@ -1,4 +1,5 @@
 import Nav from "../components/Nav/Nav";
+import UserPartRow from "../components/UserPartRow/UserPartRow";
 
 // Apollo Client imports for fetching data
 import { gql, useQuery } from "@apollo/client";
@@ -11,10 +12,12 @@ const GET_USERPARTS = gql`
     userParts {
       id
       name
-      datePurchased
       buildIdList {
         name
       }
+      datePurchased
+      purchasedPrice
+      purchasedFrom
     }
   }
 `;
@@ -30,7 +33,33 @@ function userParts() {
       {!loading && !error && (
         <>
           <Nav />
-          {data.userParts.map((userPart: any) => (<header className="App-header" key={userPart.id}>{userPart.name}</header>))}
+          <div className="p-5 h-screen bg-slate-900">
+            <h1 className="text-xl mb-2">Your Parts</h1>
+
+            <div className="overflow-auto rounded-lg shadow">
+              <div className="bg-gray-200 p-5 flex justify-end">
+                <button className="p-2 bg-gray-400 rounded hover:bg-black text-white">
+                  Add Part
+                </button>
+              </div>
+              <table className="w-full text-center">
+                <thead className="bg-gray-50 border-b-2 border-gray-200">
+                  <tr>
+                    <th className="w-32 p-3">Part Name</th>
+                    <th className="w-32 p-3">Builds Part is In</th>
+                    <th className="w-32 p-3">Date Purchased</th>
+                    <th className="w-20 p-3">Purchase Price</th>
+                    <th className="w-32 p-3">Purchased From</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.userParts.map((userPart: any) => (
+                    <UserPartRow userPart={userPart}></UserPartRow>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </>
       )}
     </div>
